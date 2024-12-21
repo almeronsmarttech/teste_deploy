0904f8a693f24bbf2ec9da40edb54c515838fb13
Mauricio Almeron <almeron@almeronsmarttech.com.br>
2024-12-21 14:16:00 -0300

    Inserindo página de contatos funcional.

--- apps/app/urls.py
+++ apps/app/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import indexView
+from .views import IndexView, Contato1View #,  ContactView
 
 urlpatterns = [
-   path('', indexView.as_view(), name='IndexView'),
+   path('', IndexView.as_view(), name='IndexView'),
+   path('contato/', Contato1View.as_view(), name='ContatoView'),
    # AllAuth Override default postlogin action with our view
-   path('accounts/profile/', indexView.as_view(), name='profileOverridenView'),
+   path('accounts/profile/', IndexView.as_view(), name='profileOverridenView'),
    path('calculadora/', include('apps.calculadora.urls')),
 ]
--- apps/app/views.py
+++ apps/app/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, FormView
+from pyexpat.errors import messages
+from django.http import JsonResponse
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
+
+from .forms import Contato1Form
 
 # Create your views here.
-class indexView(TemplateView):
+class IndexView(TemplateView):
     template_name = "app/index.html"
 
     def get_context_data(self, **kwargs):
@@ -12,7 +18,29 @@ class indexView(TemplateView):
         if self.request.user.is_authenticated:
             #context['message'] = f"Bem-vindo, {self.request.user.username}! Você está logado."
             context['logado'] = True
+            context['nome_completo'] = self.request.user.first_name.capitalize() + " " + self.request.user.last_name.capitalize()
         else:
             context['logado'] = False
             #context['message'] = "Você não está logado."
         return context
+
+
+class Contato1View(FormView):
+    template_name = 'app/contato.html'
+    form_class = Contato1Form
+    success_url = reverse_lazy('ContatoView')
+
+    def get_context_data(self, **kwargs):
+        context = super(Contato1View, self).get_context_data(**kwargs)
+        context['latitude'] = -29.1662
+        context['longititude'] = -51.1797
+        return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso')
+        return super(Contato1View, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar e-mail')
+        return super(Contato1View, self).form_invalid(form, *args, **kwargs)
