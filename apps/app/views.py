15a19532fa0a406acf7fbf06b0366b93b6b93178
Mauricio Almeron <almeron@almeronsmarttech.com.br>
2024-12-22 09:19:02 -0300

    Pequenos ajustes html corrigidos.

--- apps/app/views.py
+++ apps/app/views.py
@@ -18,7 +18,7 @@ class IndexView(TemplateView):
         if self.request.user.is_authenticated:
             #context['message'] = f"Bem-vindo, {self.request.user.username}! Você está logado."
             context['logado'] = True
-            context['nome_completo'] = self.request.user.first_name + " " + self.request.user.last_name
+            context['nome_completo'] = self.request.user.first_name.title() + " " + self.request.user.last_name.title()
         else:
             context['logado'] = False
             #context['message'] = "Você não está logado."
