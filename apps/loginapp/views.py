2703d0ad62caf03afa3eeabef91c021ffda29c4f
Mauricio Almeron <almeron@almeronsmarttech.com.br>
2025-02-24 08:07:21 -0300

    Mudando a interface

--- apps/loginapp/views.py
+++ apps/loginapp/views.py
@@ -2,4 +2,5 @@ from django.views.generic import TemplateView
 
 
 class FormPartialView(TemplateView):
-    template_name = 'loginapp/form.html'
+    #template_name = 'loginapp/form.html'
+    template_name = 'loginapp/login.html'
