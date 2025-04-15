f98bdd81736e0a78c6ba729d7616d0c7f0958da0
Mauricio Almeron <almeron@almeronsmarttech.com.br>
2025-04-15 11:29:47 -0300

    Desabilitando o login por e-mail e via google.

--- src/views.py
+++ src/views.py
@@ -2,6 +2,11 @@ from django.views.generic import TemplateView
 from django.utils.timezone import now
 from django.shortcuts import render
 from django.views import View
+import os
+import random
+from django.conf import settings
+
+
 
 class BaseView(TemplateView):
     #template_name = 'src/base.html'
@@ -14,6 +19,20 @@ class BaseView(TemplateView):
 
 
 class HomePartialView(TemplateView):
-    #template_name = 'src/home.html'
+    ##template_name = 'src/home.html'
     template_name = 'home.html'
 
+#class HomePartialView(TemplateView):
+#    template_name = "home_partial.html"
+
+#    def get_context_data(self, **kwargs):
+#        context = super().get_context_data(**kwargs)
+
+#        pasta = os.path.join(settings.STATICFILES_DIRS[0], "imagens")
+#        imagens_disponiveis = [
+#            img for img in os.listdir(pasta)
+#            if img.lower().endswith(('.jpg', '.jpeg', '.png', '.webp'))
+#        ]
+
+#        context["imagens"] = random.sample(imagens_disponiveis, 4) if len(imagens_disponiveis) >= 4 else imagens_disponiveis
+#        return context
