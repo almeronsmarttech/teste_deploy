522270bd6afaba6e611add07f6c23168da964897
Almeron SmartTech <almeron@almeronsmarttech.com.br>
2025-05-18 22:06:33 -0300

    criando classe Vigas, separando normas e criando as respectivas classes.

--- domain/structure/pavimento.py
+++ domain/structure/pavimento.py
@@ -1,10 +1,11 @@
 class Pavimento:
-    def __init__(self, nome = '', fck=None, fyk=None, caa=None):
+    def __init__(self, nome = '', nivel = 0, fck=None, fyk=None, caa=None):
         self.nome = nome
         self.fck = fck
         self.fyk = fyk
         self.caa = caa
-        self.elements = []
+        self.nivel = nivel
+        self.elementos = []
 
     def set_defaults_from_structure(self, estrutura):
         self.fck = self.fck or estrutura.fck
@@ -13,4 +14,4 @@ class Pavimento:
 
     def add_element(self, element):
         element.set_context_from_floor(self)
-        self.elements.append(element)
+        self.elementos.append(element)
